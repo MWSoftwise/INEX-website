5fdec7fd35a9bdeec1981fda82cd437d738c9b01
MWSoftwise <mwsoftwise@gmail.com>
2024-04-05 18:12:42 +0200

    Adding graphql connection in progres

--- src/app/realizacje/page.js
+++ src/app/realizacje/page.js
@@ -1,33 +1,62 @@
 'use client'
+import { useEffect } from 'react';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import PostCard from "../components/PostCard";
 import PostCardBig from "../components/PostCardBig";
 
-const url =`${process.env.BASE_URL}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}`
+export default function Projects() {
 
-export default async function Projects() {
+const url = 'https://graphql.contentful.com/content/v1/spaces/462g3ualxm53/explore?access_token';
+const TOKEN = 'cQwSYnVSVgPNjSSl1MwCkbDdIzNaSnAzsKD6c81A1Eo'
 
-    const res = await fetch(url)
-    const data = await res.json()
-
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-col items-center gap-10 mt-[100px] pt-[100px] pb-[100px] px-[15px] bg-custom">
-                <h3 className="text-5xl lg:text-6xl font-semibold text-center leading-relaxed">Sprawdź nasze realizacje</h3>
-            </div>
-            <div className="max-w-[1200px] px-[15px] flex flex-col items-center gap-10">
-                <PostCardBig />
-                <div className="w-full flex flex-wrap items-center justify-between gap-10">
-                    {
-                        data.items.map((item, index) => (
-                            <PostCard key={index} title={item.fields.title} desc={item.fields.description}  />
-                        ))
-                    }
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+const query =  `
+{
+  realizacjaCollection {
+    items {
+      slug
+      title
+      description
+      thumbnail {
+        url
+      }
+    }
   }
+}
+`;
+
+  useEffect(() => {
+
+     fetch(url, {
+        method: 'POST',
+        headers: {
+             'Content-Type': 'application/json',
+             'Authorization': `Bearer ${TOKEN}`
+            },
+        body: JSON.stringify({
+          query,
+        }),
+      })
+
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center gap-10 mt-[100px] pt-[100px] pb-[100px] px-[15px] bg-custom">
+          <h3 className="text-5xl lg:text-6xl font-semibold text-center leading-relaxed">Sprawdź nasze realizacje</h3>
+      </div>
+      <div className="max-w-[1200px] px-[15px] flex flex-col items-center gap-10">
+          <PostCardBig />
+          <div className="w-full flex flex-wrap items-center justify-between gap-10">
+
+          </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
