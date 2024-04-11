5265826cec906a166e25ca38378c59a121820286
MWSoftwise <mwsoftwise@gmail.com>
2024-04-11 13:17:02 +0200

    Change about text

--- src/app/components/AboutUs.js
+++ src/app/components/AboutUs.js
@@ -7,7 +7,7 @@ const AboutUs = () => {
             <Image src={Logotyp} alt="Logotyp" className="w-[150px] lg:w-[460px]" />
             <div className="flex flex-col gap-8 bg-custom py-10 lg:max-w-[520px]">
                 <h3 className="text-5xl lg:text-6xl font-semibold">Czym się zajmujemy?</h3>
-                <p className="text-2xl font-light lg:font-medium leading-10">Specjalizujemy się w iniekcjach budowlanych, stabilizacji gruntu oraz bezinwazyjnych naprawach obiektów. Nasze doświadczenie obejmuje remonty budynków przemysłowych i mieszkalnych, gdzie dbamy o solidność i bezpieczeństwo konstrukcji.</p>
+                <p className="text-2xl font-light lg:font-medium leading-10">Specjalizujemy się w iniekcjach budowlanych, stabilizacji gruntu oraz bezinwazyjnych naprawach obiektów. Nasze doświadczenie obejmuje remonty budynków przemysłowych i mieszkalnych, gdzie dbamy o solidność i szczelność konstrukcji.</p>
             </div>
         </div>
     )
