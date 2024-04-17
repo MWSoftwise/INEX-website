e52a950c5eb88d1b383d1fec146acc01c4ebff9c
MWSoftwise <mwsoftwise@gmail.com>
2024-04-17 12:39:16 +0200

    Fixed bug and added policy

--- src/app/components/MethodWithoutImg.js
+++ src/app/components/MethodWithoutImg.js
@@ -1,8 +1,8 @@
 const MethodWithoutImg = ({direction="flex-col lg:flex-row", title, text}) => {
     return(
-        <div className={`flex ${direction} items-center gap-10 lg:gap-40 max-w-[600px] lg:max-w-[1270px] px-[15px] mt-32 lg:justify-between`}>
-                <h3 className="text-5xl lg:text-6xl font-semibold lg:ml-8 break-all">{title}</h3>
-                <p className="text-2xl font-light lg:font-medium leading-10 max-w-[520px]">{text}</p>
+        <div className={`flex ${direction} items-center gap-10 lg:gap-40 max-w-full lg:max-w-[1270px] px-[15px] mt-32 lg:justify-between`}>
+                <h3 className="text-5xl lg:text-6xl font-semibold break-words md:break-normal max-w-full lg:max-w-[45%]">{title}</h3>
+                <p className="text-2xl font-light lg:font-medium leading-10 lg:max-w-[45%]">{text}</p>
         </div>
     )
 }
